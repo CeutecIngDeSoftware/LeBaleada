b73e46a050f71221e7d3ea441f3488c609af9d28
Nestor <Nestor>
2015-11-03 16:38:37 -0600

    Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :follow_restaurants
   resources :follow_users
   resources :foods
   resources :restaurant_reviews
   resources :food_reviews
   resources :restaurants
-  resources :users
   get 'home/index'
 
   root 'home#index'
